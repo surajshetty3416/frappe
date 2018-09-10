87e493d70a658a7d97f3403aa6cc87960a7e6e84
Suraj Shetty <surajshetty3416@gmail.com>
2018-09-10 10:04:55 +0530

    Move update user user energy point code to social profile

--- frappe/core/doctype/social_profile/social_profile.py
+++ frappe/core/doctype/social_profile/social_profile.py
@@ -5,6 +5,19 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
 
 class SocialProfile(Document):
 	pass
+
+def update_user_energy_point(point, user=None):
+	point = cint(point)
+	if not point: return
+	# TODO: find alternative
+	if user == 'admin@erpnext.com': user = 'Administrator'
+	if not user: user = frappe.session.user
+	previous_point = frappe.db.get_value('Social Profile', user, 'energy_point')
+	new_point = previous_point + point
+	frappe.db.set_value('Social Profile', user, 'energy_point', new_point)
+
+	print('================= {} gained {} points ==================='.format(user, point))
