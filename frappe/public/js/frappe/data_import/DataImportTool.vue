10b56bbc4910390d0aa23674904d62d42df0686f
Suraj Shetty <surajshetty3416@gmail.com>
2019-01-04 16:51:40 +0530

    [WIP] tool

--- frappe/public/js/frappe/data_import/DataImportTool.vue
+++ frappe/public/js/frappe/data_import/DataImportTool.vue
@@ -1,17 +1,27 @@
 <template>
 	<div>
-		<h2>Upload File</h2>
-		<label for="">
-			Select the doctype to import
-			<input type="text">
+		<label>
+			Document Type:
+			<input type="text" v-model="doctype" :disabled="doctype_verified" name="doctype" id="">
+			<button :disabled="doctype_verified" @click="set_doctype">Submit</button>
 		</label>
-		<div class="upload-box">
-			<input type="file" @change="load_file" accept=".csv, .xls" class="input-file"/>
-			<p>
-				Drag your file(s) here to begin<br> or click to browse
-			</p>
-		</div>
-		<div ref="preview"></div>
+		<input type="file" @change="load_file" accept=".csv, .xls" class="input-file"/>
+		<table v-if="docfields.length">
+			<thead>
+				<th>Doctype Field</th>
+				<th>File Column</th>
+			</thead>
+			<tbody>
+				<tr v-for="df in docfields" :key="df">
+					<td>{{df}}</td>
+					<td>
+						<select>
+							<option v-for="op in file_fields" :key="op">{{op}}</option>
+						</select>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </template>
 <script>
@@ -19,9 +29,23 @@ import DataTable from 'frappe-datatable';
 
 export default {
 	data() {
-		return {}
+		return {
+			file_fields: [],
+			docfields: [],
+			doctype_verified: false,
+			doctype: null
+		}
 	},
 	methods: {
+		set_doctype() {
+			this.doctype_verified = true
+			frappe.model.with_doctype(this.doctype)
+				.then(() => {
+					this.docfields = this.get_fields(this.doctype);
+				}).fail(() => {
+					this.doctype_verified = false
+				});
+		},
 		load_file(ev) {
 			const user_csv_file = ev.target.files[0];
 			const reader = new FileReader();
@@ -29,12 +53,20 @@ export default {
 			reader.onload = (data) => {
 				const csv = frappe.utils.csv_to_array(reader.result)
 				const head = csv.shift()
-				console.log(head, csv)
-				let datatable = new DataTable(this.$refs.preview, {
-					columns: head,
-					data: csv
-				})
+				this.file_fields = head;
+
+				// console.log(head, csv)
+				// let datatable = new DataTable(this.$refs.preview, {
+					// 	columns: head,
+				// 	data: csv
+				// })
 			}
+		},
+		filter_fields(df) {
+			return frappe.model.is_value_type(df) && !df.hidden
+		},
+		get_fields(dt) {
+			return frappe.meta.get_docfields(dt).filter(this.filter_fields).map(f => f.label)
 		}
 	}
 }
@@ -51,13 +83,13 @@ export default {
 	cursor: pointer;
 }
 
-.input-file {
-	opacity: 0; /* invisible but it's there! */
-	width: 100%;
-	height: 200px;
-	position: absolute;
-	cursor: pointer;
-}
+// .input-file {
+// 	opacity: 0; /* invisible but it's there! */
+// 	width: 100%;
+// 	height: 200px;
+// 	position: absolute;
+// 	cursor: pointer;
+// }
 
 .upload-box p {
 	text-align: center;
