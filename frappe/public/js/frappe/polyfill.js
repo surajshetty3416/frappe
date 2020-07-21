311bd4d6341cba7a790a187f8dde4d49a90e3bcb
Suraj Shetty <surajshetty3416@gmail.com>
2020-07-21 09:07:56 +0530

    feat: Add endswith polyfill

--- frappe/public/js/frappe/polyfill.js
+++ frappe/public/js/frappe/polyfill.js
@@ -81,3 +81,13 @@ if (typeof Object.assign != 'function') {
 		configurable: true
 	});
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
